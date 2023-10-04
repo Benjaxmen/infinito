607f67d9564f9c7fbf9fb789dca7d49653f63f8f
Benjaxmen <b.suarezchavarria@uandresbello.edu>
2023-10-04 19:06:21 -0300

    adsf

--- src/services/user.service.spec.ts
+++ src/services/user.service.spec.ts
@@ -70,6 +70,49 @@ describe('UserService', () => {
       await expect(service.create(userData)).rejects.toThrow(BadRequestException);
     });
   });
+  it('should throw BadRequestException if email is already associated with an account', async () => {
+    const userData = {
+      email: 'test2@example.com',
+      name: "Benjamín Suárez Chavarría",
+      password: "123123123",
+      dateofbirth: "1997-07-22",
+      profession: "ingeniero",
+      rut: "154326750",
+      cellphone: "3445564465"
+    };
+
+    jest.spyOn(userModel, 'findOne').mockResolvedValue(userData);
+
+    await expect(service.create(userData)).rejects.toThrow(BadRequestException);
+  });
+
+  it('should throw BadRequestException if email is not valid', async () => {
+    const userData = {
+      email: 'invalidemail',
+      name: "Benjamín Suárez Chavarría",
+      password: "123123123",
+      dateofbirth: "1997-07-22",
+      profession: "ingeniero",
+      rut: "154326750",
+      cellphone: "3445564465"
+    };
+
+    await expect(service.create(userData)).rejects.toThrow(BadRequestException);
+  });
+
+  it('should throw BadRequestException if user registry is not complete', async () => {
+    const userData = {
+      email: 'test2@example.com',
+      name: "Benjamín Suárez Chavarría",
+      password: "123123123",
+      dateofbirth: "1997-07-22",
+      profession: "ingeniero",
+      rut: "154326750",
+      // missing field: cellphone
+    };
+
+    await expect(service.create(userData)).rejects.toThrow(BadRequestException);
+  });
 
   // Add more test cases for other UserService methods
 
