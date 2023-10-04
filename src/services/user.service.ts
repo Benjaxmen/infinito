819351190cd98b8a9f7a327ee8f0ef0309f93bf5
Benjaxmen <90716345+Benjaxmen@users.noreply.github.com>
2023-10-04 18:51:48 -0300

    Revert "Update user services and its testing"

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,33 +1,28 @@
 import * as mongoose from 'mongoose';
 import UserSchema from '../schemas/user.schema';
 import { BadRequestException } from '@nestjs/common';
-import { BadRequestException } from '@nestjs/common';
 import { isEmail } from 'validator';
 
 class UserService {
   private userModel = mongoose.model('User', UserSchema);
 
   async create(userData) {
-      const existingUser = await this.userModel.findOne({ email: userData.email });
-      if (existingUser) {
-        throw new BadRequestException('Algo salió mal', { cause: new Error(), description: 'Ese correo electrónico ya está asociado a una cuenta' })
-      }
-      if(!isEmail(userData.email)){
-        throw new BadRequestException('Algo salió mal', { cause: new Error(), description: 'Ingresa un correo electrónico válido' })
-      }
-    
-      if (Object.values(userData).some(value => !value)) {
-        throw new BadRequestException('Algo salió mal', { cause: new Error(), description: 'Todos los campos son requeridos' });
-      }
-    
-      const user = new this.userModel({
-        ...userData,
-        dateOfRegistry: Date.now(),
-        rol: 'Usuario',
-      });
-      await user.save();
-      return user;
+    const existingUser = await this.userModel.findOne({ email: userData.email });
+    if (existingUser) {
+      throw new BadRequestException('Algo salió mal', { cause: new Error(), description: 'Ese correo electrónico ya está asociado a una cuenta' })
     }
+    if(!isEmail(userData.email)){
+      throw new BadRequestException('Algo salió mal', { cause: new Error(), description: 'Ingresa un correo electrónico válido' })
+
+    }
+
+    const user = new this.userModel(userData);
+    await user.save();
+    return user;
+  }
+
+  async findAll() {
+    const users = await this.userModel.find();
     return users;
   }
 
